684c11106f6bbab9214ba1eb514271ea748ecf7a
VuralErdem <vural.erdem@protonmail.com>
2022-01-26 22:34:48 +0100

    collision and score feature implemented

--- main.py
+++ main.py
@@ -1,19 +1,42 @@
 import pygame
 import sys
 import time
+import random
 from pygame.locals import *
 
 SIZE = 40
 
+class Apple:
+    
+    def __init__(self, parent_screen):
+        self.image = pygame.image.load("resources/apple.jpg").convert()
+        self.parent_screen = parent_screen
+        self.x = SIZE*3
+        self.y = SIZE*3
+        
+    def draw(self):
+        self.parent_screen.blit(self.image,(self.x, self.y))
+        pygame.display.flip()
+        
+    def move(self):
+        self.x = random.randint(0, 24)*SIZE
+        self.y = random.randint(0, 19)*SIZE
+    
+
 class Snake:
     
-    def __init__(self, surface, length):
+    def __init__(self, parent_screen, length):
         self.length = length
-        self.parent_screen = surface
+        self.parent_screen = parent_screen
         self.block = pygame.image.load("resources/block.jpg").convert()
         self.x = [SIZE]*length
         self.y = [SIZE]*length
         self.direction = "down"
+        
+    def increase_length(self):
+        self.length += 1
+        self.x.append(-1)
+        self.y.append(-1)
         
     
     def move_left(self):
@@ -40,15 +63,16 @@ class Snake:
         
         for i in range(self.length-1,0, -1):
             self.x[i] = self.x[i-1]
-            self.y[i] = self.y[i-1]        
-            if self.direction == "left":
-                self.x[0] -= 10
-            if self.direction == "right":
-                self.x[0] += 10
-            if self.direction == "up":
-                self.y[0] += 10
-            if self.direction == "down":
-                self.y[0] -= 10
+            self.y[i] = self.y[i-1]     
+               
+        if self.direction == "left":
+                self.x[0] -= SIZE
+        if self.direction == "right":
+                self.x[0] += SIZE
+        if self.direction == "up":
+                self.y[0] += SIZE
+        if self.direction == "down":
+                self.y[0] -= SIZE
                 
         self.draw()
             
@@ -63,8 +87,36 @@ class Game:
         self.surface.fill((100, 110, 5))
         self.snake = Snake(self.surface, 2)
         self.snake.draw()
+        self.apple = Apple(self.surface)
+        self.apple.draw()
         
         
+    def is_collision(self, x1, y1, x2, y2):
+        if x1 >= x2 and x1 <= x2 + SIZE:
+            if y1 >= y2 and y1 <= x2 + SIZE:
+                return True
+        
+        return False
+        
+        
+    def play(self):
+        self.snake.walk()
+        self.apple.draw()
+        self.display_score()
+        pygame.display.flip()
+        
+        if self.is_collision(self.snake.x[0],self.snake.y[0], self.apple.x, self.apple.y):
+            self.snake.increase_length()
+            self.apple.move()
+            
+            
+    def display_score(self):
+        font = pygame.font.SysFont("arial", 30)
+        score = font.render(f"Score: {self.snake.length}", True, (200,200,200))
+        self.surface.blit(score, (800, 10))
+        
+        
+    
         
     def run(self):
         
@@ -89,8 +141,9 @@ class Game:
                 elif event.type == pygame.QUIT:
                     sys.exit()
                     
-            self.snake.walk()
-            time.sleep(0.2)
+            self.play()
+            
+            time.sleep(0.1)
   
 
 
